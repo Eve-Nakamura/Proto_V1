ea0e09a610e2b959e121dcbc29a997df7ef941e6
Eveee <73877020+Eve-Nakamura@users.noreply.github.com>
2021-11-17 23:52:33 +0100

    effet typewriter sur les dialogues ma gueule

---
+++ Assets/Script/TypeWriter.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TypeWriter : MonoBehaviour
+{
+    public string fullText;
+    private string currentText;
+    public float delay = 0.2f;
+
+    void Start() //avant même que la scène ne démarre pour pas qu'on voit
+    {
+        StartCoroutine(ShowText());
+    }
+
+    IEnumerator ShowText()
+    {
+        for (int i = 0; i< fullText.Length; i++)
+        {
+            currentText = fullText.Substring(0, i);
+            this.GetComponent<TMPro.TextMeshProUGUI>().text = currentText;
+            yield return new WaitForSeconds(delay);
+        }
+    }
+
+}
