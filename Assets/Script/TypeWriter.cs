33b045807aef62d78413aae0c1ecb4905e9bcefb
Eveee <tom.paletou@gmail.com>
2021-11-29 17:36:23 +0100

    gros porc de texte

--- Assets/Script/TypeWriter.cs
+++ Assets/Script/TypeWriter.cs
@@ -1,31 +1,79 @@
 using System.Collections;
 using UnityEngine;
+using UnityEngine.UI;
 using TMPro;
 
 public class TypeWriter : MonoBehaviour
 {
     public string fullText;
-    private string currentText;
+    public string currentText;
     public float delay = 0.2f;
-    
+    public bool clickReady = false;
+    public bool finishText = false;
 
+    [SerializeField] UI_Manager MainExit;
+    [SerializeField] UI_Manager Codex;
+    [SerializeField] UI_Manager Carte;
+    [SerializeField] UI_Manager Journal;
+    [SerializeField] UI_Manager Option;
+    [SerializeField] UI_Manager Perso;
+    [SerializeField] UI_Manager Fond;
+    [SerializeField] UI_Manager MissClick;
 
-    void Start() //avant même que la scène ne démarre pour pas qu'on voit
+    void Start() 
     {
         StartCoroutine(ShowText());
     }
 
+    public void stopCoroutine()
+    {
+        clickReady = true;
+        StopCoroutine(ShowText());
+    }
     IEnumerator ShowText()
     {
         for (int i = 0; i< fullText.Length; i++)
         {
-            if (!Input.GetMouseButtonDown(0))
+            if (!Input.GetMouseButtonDown(0) && !clickReady)
             {
                 currentText = fullText.Substring(0, i);
                 GetComponent<TextMeshProUGUI>().text = currentText;
                 yield return new WaitForSeconds(delay);
             }
-               
+        }
+        if (fullText == currentText)
+        {
+            finishText = true;
+            // Ajout toutes les fonctions
+            if (transform.parent.GetComponent<UI_Manager>().dialFin)
+            {
+                transform.parent.GetComponent<Button>().onClick.AddListener(MainExit.display);
+                transform.parent.GetComponent<Button>().onClick.AddListener(Codex.display);
+                transform.parent.GetComponent<Button>().onClick.AddListener(Carte.display);
+                transform.parent.GetComponent<Button>().onClick.AddListener(Journal.display);
+                transform.parent.GetComponent<Button>().onClick.AddListener(Option.display);
+                transform.parent.GetComponent<Button>().onClick.AddListener(Perso.minimizePerso);
+                transform.parent.GetComponent<Button>().onClick.AddListener(Fond.hide);
+                transform.parent.GetComponent<Button>().onClick.AddListener(MissClick.hide);
+            }
+        }
+    }
+
+    public void displayText()
+    {
+        currentText = fullText;
+        GetComponent<TextMeshProUGUI>().text = currentText;
+        // Ajout toutes les fonctions
+        if (transform.parent.GetComponent<UI_Manager>().dialFin)
+        {
+            transform.parent.GetComponent<Button>().onClick.AddListener(MainExit.display);
+            transform.parent.GetComponent<Button>().onClick.AddListener(Codex.display);
+            transform.parent.GetComponent<Button>().onClick.AddListener(Carte.display);
+            transform.parent.GetComponent<Button>().onClick.AddListener(Journal.display);
+            transform.parent.GetComponent<Button>().onClick.AddListener(Option.display);
+            transform.parent.GetComponent<Button>().onClick.AddListener(Perso.minimizePerso);
+            transform.parent.GetComponent<Button>().onClick.AddListener(Fond.hide);
+            transform.parent.GetComponent<Button>().onClick.AddListener(MissClick.hide);
         }
     }
 
--- Assets/Script/UI_Manager.cs
+++ Assets/Script/UI_Manager.cs
@@ -8,20 +8,60 @@ public class UI_Manager : MonoBehaviour
 
     public Vector3 initPosition;
     public Vector3 initScale;
+    public bool dialFin;
+
+    bool readyBouton = false;
+    bool mini;
 
     private void Start()
     {
-        initPosition = gameObject.transform.position;
         initScale = gameObject.transform.localScale;
+        initPosition = gameObject.transform.position;
+        mini = false;
     }
     public void display()
     {
-        gameObject.SetActive(true);
+        GameObject[] dialList = GameObject.FindGameObjectsWithTag("Dial");
+        int nbrList = dialList.Length;
+        int nbrHide = 0;
+        foreach (GameObject item in dialList)
+        {
+            if (item.activeSelf == false)
+            {
+                nbrHide++;
+            }
+        }
+
+
+        if (nbrHide == nbrList)
+        {
+            readyBouton = true;
+            gameObject.SetActive(true);
+        }
     }
 
     public void hide()
     {
-        gameObject.SetActive(false);
+        if (gameObject.tag == "Dial")
+        {
+            GameObject click = gameObject.transform.GetChild(0).gameObject;
+
+            if (click.GetComponent<TypeWriter>().currentText != click.GetComponent<TypeWriter>().fullText)
+            {
+                click.GetComponent<TypeWriter>().stopCoroutine();
+                click.GetComponent<TypeWriter>().displayText();
+            }
+
+            else if (click.GetComponent<TypeWriter>().currentText == click.GetComponent<TypeWriter>().fullText)
+            {
+
+                gameObject.SetActive(false);
+            }
+        }
+        else
+        {
+            gameObject.SetActive(false);
+        }
     }
 
     [System.Obsolete]
@@ -37,17 +77,21 @@ public class UI_Manager : MonoBehaviour
             gameObject.SetActive(true);
         }
     }
-
     public void minimizePerso()
     {
-        gameObject.transform.localPosition = new Vector3(740, -150, initPosition.z);
-        gameObject.transform.localScale = new Vector3(gameObject.transform.localScale.x/3, gameObject.transform.localScale.y/3, gameObject.transform.localScale.z);
-       
+        print("minimize perso");
+        if (!mini)
+        {
+            mini = true;
+            gameObject.transform.localPosition = new Vector3(740, -150, initPosition.z);
+            gameObject.transform.localScale = new Vector3(gameObject.transform.localScale.x / 3, gameObject.transform.localScale.y / 3, gameObject.transform.localScale.z);
+        }
     } 
     public void rescalePerso()
     {
-        gameObject.transform.localScale = new Vector3(initScale.x, initScale.y, gameObject.transform.localScale.z);
+        mini = false;
+        gameObject.transform.localScale = new Vector3(1, 1, 0f);
         gameObject.transform.position = new Vector3(initPosition.x, initPosition.y, gameObject.transform.position.z);
-    }
 
+    }
 }
