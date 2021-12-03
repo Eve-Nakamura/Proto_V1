6467842b745e7dae7da362cc055bf390eb352304
lydcat <evacabana@live.fr>
2021-12-03 15:07:57 +0100

    update_matabiau
    script
    map

---
+++ Assets/Script/Show_Verrou.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Show_Verrou : MonoBehaviour
+{
+    public GameObject image1;
+    public GameObject journal;
+    public GameObject options;
+    public GameObject Fond;
+    void Start()
+    {
+
+    }
+
+    // c'est pareil que les minimes mais pr le chateau de brax
+    [System.Obsolete]
+    void Update()
+    {
+        Ray ray;
+        RaycastHit hit;
+
+        ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        if (Physics.Raycast(ray, out hit))
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                if (hit.collider.name == "Matabiau")
+                {
+
+                    image1.GetComponent<UI_Manager>().display();
+                    Fond.GetComponent<UI_Manager>().display();
+                    journal.GetComponent<UI_Manager>().hide();
+                    options.GetComponent<UI_Manager>().hide();
+
+                }
+
+            }
+        }
+    }
+}
+
